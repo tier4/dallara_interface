7ee515d3088d9c6bb924b34eb0912a380cbb3fe0
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2023-11-03 01:02:59 +0900

    feat: path to trajectory twist stamped (#12)
    * feat: add path_to_trajectory
    
    * feat: add twist stamped
    
    * fix: exe node
    
    * feat: add twist 2 accel node launch
    
    * feat: update rviz
    
    * chore: add control
    
    * fix: sim time order
    
    * fix: separate ll2
    
    * fix: global option map
    
    * feat: add control and api rviz
    
    * fix(dallara_interface): unit topic name
    
    * feat: add rviz
    
    * fix: velocity
    
    ---------
    
    Co-authored-by: Y.Hisaki <yhisaki31@gmail.com>

--- awsim_adapter/odom2tf/include/odom2tf/odom2tf.hpp
+++ awsim_adapter/odom2tf/include/odom2tf/odom2tf.hpp
@@ -20,10 +20,12 @@
 #include <tf2_ros/transform_listener.h>
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
 
 using nav_msgs::msg::Odometry;
 using tf2_msgs::msg::TFMessage;
 using geometry_msgs::msg::TransformStamped;
+using geometry_msgs::msg::TwistWithCovarianceStamped;
 
 class Odom2tf : public rclcpp::Node {
 public:
@@ -32,6 +34,9 @@ public:
 private:
   void odomCallback(const Odometry::SharedPtr odometry);
 
+
   rclcpp::Subscription<Odometry>::SharedPtr sub_odom_;
   rclcpp::Publisher<TFMessage>::SharedPtr pub_tf_;
+  rclcpp::Publisher<TwistWithCovarianceStamped>::SharedPtr pub_twist_;
+
 };
